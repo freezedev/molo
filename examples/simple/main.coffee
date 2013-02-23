d335f00507dc9e65c8680eaa2fa66fa1de2233ca
Johannes Stein <johannesstein@freeze-dev.com>
2013-02-23 20:42:02 +0100

    Updated simple example
    Due to how script file loading works, multiple module definitions in the
    same file need to be in the correct order

--- examples/simple/main.coffee
+++ examples/simple/main.coffee
@@ -1,7 +1,13 @@
-module 'test'
-  require: ['testfun', 'teststring']
-  define: (testfun, teststring) -> testfun teststring
+###
+If all the modules are in the same file, you need
+to take care of the correct order for these 
+dependencies yourself
+###
 
 module 'teststring', -> 'test'
 
 module 'testfun', -> (param) -> alert param
+
+module 'test'
+  require: ['testfun', 'teststring']
+  define: (testfun, teststring) -> testfun teststring
--- examples/simple/main.js
+++ examples/simple/main.js
@@ -1,11 +1,12 @@
-(function() {
 
-  module('test', {
-    require: ['testfun', 'teststring'],
-    define: function(testfun, teststring) {
-      return testfun(teststring);
-    }
-  });
+/*
+If all the modules are in the same file, you need
+to take care of the correct order for these 
+dependencies yourself
+*/
+
+
+(function() {
 
   module('teststring', function() {
     return 'test';
@@ -15,6 +16,13 @@
     return function(param) {
       return alert(param);
     };
+  });
+
+  module('test', {
+    require: ['testfun', 'teststring'],
+    define: function(testfun, teststring) {
+      return testfun(teststring);
+    }
   });
 
 }).call(this);
